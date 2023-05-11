964a9eb8ffb5b3dbf50d22362ad250df8f68dd8b
10samarth <samarthmg@gmail.com>
2023-05-11 13:34:55 -0700

    reports

--- src/pages/Admin/Accounts/index.jsx
+++ src/pages/Admin/Accounts/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import ChartistGraph from "react-chartist";
+ 
 function AccountsComponent() {
+ 
+
   return (
-    <div style={{ marginTop: "150px" }}>
+    <div style={{ marginTop: "-60px" , marginLeft:'-185px', zIndex:-1}}>
       <h2>Reports</h2>
-      <p>This is the Accounts component.</p>
+      <iframe
+ 
+      width="100%"
+      height="100%"
+      src="http://localhost:3001/"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      style={{
+        width:"1000px",
+        height: '100vh'}}
+    />
+        
     </div>
   );
 }
